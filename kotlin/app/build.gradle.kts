49c25e7d23f96f03f57a686efc46a362ce2f4824
tochy <tochy@openrhapsody.com>
2024-01-15 17:07:45 +0900

    v0.2.2

--- kotlin/app/build.gradle.kts
+++ kotlin/app/build.gradle.kts
@@ -45,5 +45,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("io.adrop:adrop-ads:0.2.1")
+    implementation("io.adrop:adrop-ads:0.2.2")
 }
