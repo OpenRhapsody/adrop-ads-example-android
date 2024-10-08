f1c870a47820947869c1676dd0123eca3bb9f9ab
tochy <tochy@openrhapsody.com>
2024-10-08 15:27:29 +0900

    v0.7.2

--- kotlin/SplashAdViewExample/app/build.gradle.kts
+++ kotlin/SplashAdViewExample/app/build.gradle.kts
@@ -42,5 +42,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation("io.adrop:adrop-ads:0.7.1")
+    implementation("io.adrop:adrop-ads:0.7.2")
 }
