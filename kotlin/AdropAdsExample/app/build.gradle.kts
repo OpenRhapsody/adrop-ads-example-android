322ec9e7c67f0da97e7fec07c41106803270e267
tochy <tochy@openrhapsody.com>
2024-10-10 18:16:38 +0900

    v0.7.3

--- kotlin/AdropAdsExample/app/build.gradle.kts
+++ kotlin/AdropAdsExample/app/build.gradle.kts
@@ -47,5 +47,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("io.adrop:adrop-ads:0.7.2")
+    implementation("io.adrop:adrop-ads:0.7.3")
 }
