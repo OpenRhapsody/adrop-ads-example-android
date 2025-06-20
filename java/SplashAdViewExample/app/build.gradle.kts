e0ac902119ce502475881ecec387810bc7b53951
tochy <tochy@openrhapsody.com>
2025-06-20 18:29:27 +0900

    v1.3.16

--- java/SplashAdViewExample/app/build.gradle.kts
+++ java/SplashAdViewExample/app/build.gradle.kts
@@ -38,5 +38,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation("io.adrop:adrop-ads:1.3.15")
+    implementation("io.adrop:adrop-ads:1.3.16")
 }
